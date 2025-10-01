e24fdd775ad4494943d3c3a21d5eb699882f3022
Max Ryan <161385834+maxryan4@users.noreply.github.com>
2025-10-01 22:24:16 +0100

    Added and modifiel keyboard node added setup.cfg added resource folder modified setup.py

--- src/base/keyboard_control/setup.py
+++
@@ -1,5 +0,0 @@
-entry_points={
-    'console_scripts': [
-        'keyboard_node = keyboard_control.keyboard_node:main',
-    ],
-},
--- src/base/keyboard_node/launch/keyboard_launch.py
+++ src/base/keyboard_node/launch/keyboard_launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='keyboard_control',
+            package='keyboard_node',
             executable='keyboard_node',
             name='keyboard_node',
             output='screen'
